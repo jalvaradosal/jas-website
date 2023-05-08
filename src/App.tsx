898f81693d3651708385d8b705432e8958e92f3a
jalvarados101 <jalvaradosal@wesleyan.edu>
2023-05-07 17:35:12 -0700

    adding a nav bar

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Nav from './components/Nav';
+
+function App() {
+  return (
+    <div>
+      <Nav />
+      <h1>Jo Mama</h1>
+
+    </div>
+  );
+}
+
+export default App;
